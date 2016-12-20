349061498c69e842ed1f8941d2ca8807f76e7a34
Adedotun Odutola <dotunodutola@gmail.com>
2016-12-19 22:12:35 -0600

    Add files via upload

---
+++ src/public_html/import_data.php
@@ -0,0 +1,117 @@
+<?php
+/* connect to aco0008 */
+
+$dbhost = "acadmysql.duc.auburn.edu";
+$dbuser = "aco0008";
+$dbpass = "chinedu11";
+$dbname = "aco0008db";
+
+$connect = mysql_connect($dbhost, $dbuser, $dbpass);
+
+if (!$connect) {
+ die('Could not connect to MySQL: ' . mysql_error());
+}
+
+$db = mysql_select_db($dbname, $connect);
+
+// path where your CSV file is located
+$csvBook = "./data/db_book.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvBook'
+     INTO TABLE Book
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (BookID, Title, UnitPrice, Author, Quantity, SupplierID, SubjectID)";
+
+mysql_query($query);
+
+echo "$csvBook data successfully imported to database!!\n";
+
+$csvCustomer = "./data/db_customer.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvCustomer'
+     INTO TABLE Customer
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (CustomerID, LastName, FirstName, Phone)";
+
+mysql_query($query);
+
+echo "$csvCustomer data successfully imported to database!!\n";
+
+$csvEmployee = "./data/db_employee.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvEmployee'
+     INTO TABLE Employee
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (EmployeeID, LastName, FirstName)";
+
+mysql_query($query);
+
+echo "$csvEmployee data successfully imported to database!!\n";
+
+$csvOrders = "./data/db_order.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvOrders'
+     INTO TABLE Orders
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (OrderID, CustomerID, EmployeeID, OrderDate, ShippedDate, ShipperID)";
+
+mysql_query($query);
+echo "$csvOrder data successfully imported to database!!\n";
+
+$csvOrderDetail = "./data/db_order_detail.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvOrderDetail'
+     INTO TABLE OrderDetail
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (BookID, OrderID, Quantity)";
+
+mysql_query($query);
+
+echo "$csvOrderDetail data successfully imported to database!!\n";
+
+$csvShipper = "./data/db_shipper.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvShipper'
+     INTO TABLE Shipper
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (ShipperID, ShipperName)";
+
+mysql_query($query);
+
+echo "$csvShipper data successfully imported to database!!\n";
+
+$csvSubject = "./data/db_subject.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvSubject'
+     INTO TABLE Subject
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (SubjectID, CategoryName)";
+
+mysql_query($query);
+
+echo "$csvSubject data successfully imported to database!!\n";
+
+
+$csvSupplier = "./data/db_supplier.csv";
+$query = "LOAD DATA LOCAL INFILE '$csvSupplier'
+     INTO TABLE Supplier
+     FIELDS TERMINATED BY ','
+     LINES TERMINATED BY '\n' 
+     IGNORE 1 LINES 
+     (SupplierID, CompanyName, ContactLastName, ContactFirstName, Phone)";
+
+mysql_query($query);
+
+echo "$csvSupplier data successfully imported to database!!\n";
+
+
+mysql_close($connect);
+
+?>
---
+++ src/public_html/index.php
@@ -0,0 +1,196 @@
+<html>
+	<head>
+	<title>COMP 5120</title>
+	<style>
+	table {
+    		width:100%;
+	}
+	table, th, td {
+    		border: 1px solid black;
+    		border-collapse: collapse;
+	}
+	th, td {
+    		padding: 5px;
+    		text-align: left;
+	}
+	table#style1 tr:nth-child(even) {
+    		background-color: #eee;
+	}
+	table#style1 tr:nth-child(odd) {
+   		background-color:#fff;
+	}
+	table#style1 th	{
+    		background-color: black;
+    		color: white;
+	}
+	
+	</style>
+	</head>
+	
+	<body>
+		<form action="index.php" method="post">
+			<input type="submit" name="Book" value="Book">
+			<input type="submit" name="Customer" value="Customer">
+			<input type="submit" name="Employee" value="Employee">
+			<input type="submit" name="Orders" value="Orders">
+			<input type="submit" name="OrderDetail" value="OrderDetail">
+			<input type="submit" name="Shipper" value="Shipper">
+			<input type="submit" name="Subject" value="Subject">
+			<input type="submit" name="Supplier" value="Supplier">
+	<?php
+		
+		if(isset($_POST['Book'])) {
+		
+			bookTable();
+			
+		}
+		else if(isset($_POST['Customer'])) {
+			
+			customerTable();
+			
+		}
+		else if(isset($_POST['Employee'])) {
+			
+			employeeTable();
+			
+		}
+		else if(isset($_POST['Orders'])) {
+			
+			ordersTable();
+			
+		}
+		else if(isset($_POST['OrderDetail'])) {
+			
+			orderDetailTable();
+			
+		}
+		else if(isset($_POST['Shipper'])) {
+			
+			shipperTable();
+			
+		}
+		else if(isset($_POST['Subject'])) {
+			
+			subjectTable();
+			
+			}
+		else if(isset($_POST['Supplier'])) {
+			
+			supplierTable();
+			
+		}
+		
+		if(isset($_POST['Submit'])) {
+			$sqlCommand = str_replace("\\", "", $_POST['sqlCommand']);
+			execute($sqlCommand);
+		}
+	?>
+
+	<br><br><h2>Enter SQL Command:</h2>
+	<textarea cols="50" rows="10" name="sqlCommand"></textarea>
+	<input type="submit" name="Submit" value="Submit">
+	</br></br></br>
+
+		</form>
+	</body>
+</html>
+
+	<?php
+	function execute($query){
+		$mysqli = new mysqli("acadmysql.duc.auburn.edu", "aco0008", "chinedu11");
+    		if(!$mysqli) {
+        		die();
+    		}
+    		mysqli_select_db($mysqli, "aco0008db");
+    		if(!$mysqli) {
+        		die();
+    		}
+    		if (!($res = $mysqli->query($query))) {
+        		if($mysqli) $mysqli->close();
+        		return;
+    		}
+    		echo "<table id='style1'>";	
+ 
+    		// Print the table's header row
+    		$numFields = $mysqli->field_count;
+    		echo "<tr>";
+    		$finfo = $res->fetch_fields();
+    		foreach ($finfo as $val) {
+        		echo "<th>" . $val->name . "</th>";
+    		}
+    		echo "</tr>";
+ 
+    		// Print each row
+    		$i = 0;
+    		while($row = $res->fetch_row()){
+        		echo "<tr";
+        		if ($i % 2 == 0)
+            		echo " bgcolor=\"#CCCCCC\"";
+        		echo ">";
+ 
+        		for($j = 0; $j < $numFields; $j++){
+            			echo "<td>" . $row[$j] . "</td>";
+        		}
+ 
+        		echo "</tr>";
+        		$i++;
+    		}
+ 
+    		echo "</table><br/><br/>";	
+ 
+    		// Free resources
+    		if($res) $res->free(); 		
+    		if($mysqli) $mysqli->close(); 	
+	}
+
+		
+		/*Print out Book table*/
+		function bookTable(){
+			$sql = "SELECT * FROM Book";
+			execute($sql);
+				
+		}
+
+		/*Print out Customer table*/
+		function customerTable(){
+			$sql = "SELECT * FROM Customer";
+			execute($sql);
+		}
+
+		/*Print out Employee table*/
+		function employeeTable(){
+			$sql = "SELECT * FROM Employee";
+			execute($sql);
+		}
+		
+		/*Print out Orders table*/
+		function ordersTable(){
+			$sql = "SELECT * FROM Orders";
+			execute($sql);
+		}
+		
+		/*Print out OrderDetail table*/
+		function orderDetailTable(){
+			$sql = "SELECT * FROM OrderDetail";
+			execute($sql);
+		}
+		
+		/*Print out Shipper table*/
+		function shipperTable(){
+			$sql = "SELECT * FROM Shipper";
+			execute($sql);
+		}
+
+		/*Print out Subject table*/
+		function subjectTable(){
+			$sql = "SELECT * FROM Subject";
+			execute($sql);
+		}
+
+		/*Print out Supplier table*/
+		function supplierTable(){
+			$sql = "SELECT * FROM Supplier";
+			execute($sql);
+		}
+
+	?>
